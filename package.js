077f69c2becc803558a998e5cfa7f0de3324df64
duongtdn <duongtdn@gmail.com>
2016-05-09 12:05:08 +0700

    update package with GIT link

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'stormgle:util',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Provide useful util methods',
+  summary: 'Useful utilities for Stormgle projects',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/stormgle/util.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
